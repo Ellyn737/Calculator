6f586987e7eba86e6006c83ab52c54294f99ced9
Ellyn737 <e.reese@mail.de>
2017-04-11 22:16:15 +0200

    added user input

--- Calculator.java
+++ Calculator.java
@@ -2,5 +2,10 @@ import java.util.*;
 public class Calculator{
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("Bitte eingeben [+-*/] zahl1 zahl2: ");
+		String operator = scanner.next();
+		int zahl1 = scanner.nextInt();
+		int zahl2 = scanner.nextInt();
+		
 	}
 }
